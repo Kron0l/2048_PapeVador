ba488e75426db053b493b23ab9facdb107ed1f53
Tristan BODA <115488503+Kron0l@users.noreply.github.com>
2023-10-23 13:55:57 +0200

    Update 2048_PapeVador.cpp

--- 2048_PapeVador/2048_PapeVador/2048_PapeVador/2048_PapeVador.cpp
+++ 2048_PapeVador/2048_PapeVador/2048_PapeVador/2048_PapeVador.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -8,24 +6,24 @@ using namespace std;
 int main()
 {
     
-    vector<string> row1({ "00","01","02","03" });
-    vector<string> row2({ "10","11","12","13" });
-    vector<string> row3({ "20","21","22","23" });
-    vector<string> row4({ "30","31","32","33" });
+    vector<int> row1({ 0,0,0,0});
+    vector<int> row2({ 0,0,0,0});
+    vector<int> row3({ 0,0,0,0 });
+    vector<int> row4({ 0,0,0,0 });
     
-    vector<vector<string>> grid({ row1,row2,row3,row4 });
+    vector<vector<int>> grid({ row1,row2,row3,row4 });
 
     for (int x = 0; x <= 3; x++) {
         for (int y = 0; y <= 3; y++) {
-            cout << grid[x][y] << ',';
+            if (grid[x][y] != 0) {
+                cout << grid[x][y] << ',';
+            }
+            else {
+                cout << " " << ',';
+            }
         }
         cout << endl;
     }
-
-
-    //vector<vector<int>> ti;
-
-    //cout << "\n";
      
 }
 
