ba8e355b41acd1f5465c587bebbaf03d5bc62ad2
Tristan BODA <115488503+Kron0l@users.noreply.github.com>
2023-10-23 13:41:11 +0200

    grid dynamique

--- 2048_PapeVador/2048_PapeVador/2048_PapeVador/2048_PapeVador.cpp
+++ 2048_PapeVador/2048_PapeVador/2048_PapeVador/2048_PapeVador.cpp
@@ -1,20 +1,31 @@
-// 2048_PapeVador.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+
 
 #include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    vector<string> row1({ "00","01","02","03" });
+    vector<string> row2({ "10","11","12","13" });
+    vector<string> row3({ "20","21","22","23" });
+    vector<string> row4({ "30","31","32","33" });
+    
+    vector<vector<string>> grid({ row1,row2,row3,row4 });
+
+    for (int x = 0; x <= 3; x++) {
+        for (int y = 0; y <= 3; y++) {
+            cout << grid[x][y] << ',';
+        }
+        cout << endl;
+    }
+
+
+    //vector<vector<int>> ti;
+
+    //cout << "\n";
+     
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
